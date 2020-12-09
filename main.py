bc9d3d75d422cc5407834a8f5769722635538dd4
Matthias Haasler <mhaasler@gmail.com>
2020-12-09 01:12:18 +0100

    add days package

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import day_01
-import day_02
+from days import day_01, day_02
 
 
 def print_solutions(day, task_01, task_02):
